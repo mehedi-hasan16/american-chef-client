71afe8781936e0b70ceebf7ab14dfa50b82c379c
mehedi-hasan16 <hasanmehedi6541@gmail.com>
2023-05-02 01:33:08 +0600

    added carousel

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import NavigationBar from "../../shared/NavigationBar/NavigationBar";
+import Banner from "../Banner/Banner";
 import ChefArea from "../ChefArea/ChefArea";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <NavigationBar></NavigationBar>
+            <Banner></Banner>
             <ChefArea></ChefArea>
         </div>
     );
