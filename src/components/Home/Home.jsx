aeb1179989f4101dd474fc2271b06c6c6d0c95cd
mehedi-hasan16 <hasanmehedi6541@gmail.com>
2023-05-02 00:45:50 +0600

    added navbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
+import NavigationBar from "../../shared/NavigationBar/NavigationBar";
 import ChefArea from "../ChefArea/ChefArea";
 
 const Home = () => {
    
     return (
         <div>
+            <NavigationBar></NavigationBar>
             <ChefArea></ChefArea>
         </div>
     );
--- src/shared/NavigationBar/NavigationBar.jsx
+++ src/shared/NavigationBar/NavigationBar.jsx
@@ -1,8 +1,27 @@
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 
 const NavigationBar = () => {
     return (
         <div>
-            
+            <Navbar bg="light" expand="lg" className="container">
+                <Container fluid>
+                    <Navbar.Brand href="#">American Chef</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="mx-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="#action1">Home</Nav.Link>
+                            <Nav.Link href="#action2">Blog</Nav.Link>
+                        </Nav>
+
+                            <Button variant="outline-success">Login</Button>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
