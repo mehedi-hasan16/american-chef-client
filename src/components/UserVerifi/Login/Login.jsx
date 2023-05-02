d94fa4a9f51bbb96c8e94b54f188b233fc0fc8bd
mehedi-hasan16 <hasanmehedi6541@gmail.com>
2023-05-02 23:26:46 +0600

    added login and register page

--- src/components/UserVerifi/Login/Login.jsx
+++ src/components/UserVerifi/Login/Login.jsx
@@ -1,10 +1,36 @@
+import { Button, Form } from "react-bootstrap";
+import { FaFacebook, FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
     return (
-        <div>
-            this is login page
+        <div className="vh-100">
+        <h1 className="text-center my-4">Login your account</h1>
+        <Form  className="w-25 mx-auto border rounded p-4">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" name="email" placeholder="Enter email" required />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" name="password" placeholder="Password" required />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            </Form.Group>
+            <Button variant="primary" type="submit">Login</Button><br />
+            <Form.Text>
+                Don't have an account? <Link to={'/register'}>Register</Link>
+            </Form.Text>
+        </Form>
+        <div className="d-flex flex-column align-items-center my-2">
+            <h4>Or, login with</h4>
+           <div className="mb-2"> <Button variant="outline-success"><FaFacebook/> Login with Google</Button></div>
+            <div><Button variant="outline-danger"><FaGithub/> Login with GitHub</Button></div>
         </div>
+    </div>
     );
 };
 
--- src/components/UserVerifi/Register/Register.jsx
+++ src/components/UserVerifi/Register/Register.jsx
@@ -1,9 +1,39 @@
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
     return (
-        <div>
-           this is register page 
+        <div className="vh-100">
+            <h1 className="text-center my-4">Register your account</h1>
+           <Form className="w-25 mx-auto border rounded p-4">
+           <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control type="text" name="fullName" placeholder="Enter Your Name" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Photo URL</Form.Label>
+                    <Form.Control type="text" name="photoUrl" placeholder="Photo URL" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name="email" placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Accecpt term & conditions" />
+                </Form.Group>
+                <Button variant="primary" type="submit" >Register</Button><br />
+                <Form.Text>
+                    Already have an Account? <Link to={'/login'}>Login</Link>
+                </Form.Text>
+            </Form>
         </div>
     );
 };
